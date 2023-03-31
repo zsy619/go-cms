6fc95147a86a85826523a3d67643fed4da8137ee
朱书彦 <zsy619@163.com>
2023-03-31 18:15:16 +0800

    【feat】在线报名接口

--- controllers/www/a.go
+++ controllers/www/a.go
@@ -24,6 +24,8 @@ func init() {
 	web.Router("/api/article/click", &ApiArticleController{}, "*:Click")
 	web.Router("/api/article/like", &ApiArticleController{}, "*:Like")
 
+	web.Router("/api/online/register", &ApiOnlineRegisterController{}, "*:Find")
+
 	web.Router("/api/cache/clear", &ApiCacheController{}, "*:Clear")
 
 	web.Router("/", &IndexController{}, "*:Index")
