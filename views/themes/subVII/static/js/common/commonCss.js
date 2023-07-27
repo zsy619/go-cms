32c544b9a8cd14237b92626f020387f41aeb3b83
孙孟良 <liangxxmeng@163.com>
2023-07-27 09:03:26 +0800

    【feat】 模板七、八cms接入 【fix】 模板二重命名

---
+++ views/themes/subVII/static/js/common/commonCss.js
@@ -0,0 +1,9 @@
+document.write('<link href="/static/theme/common/static/plugins/layui/css/layui.css" rel="stylesheet" type="text/css" />');
+document.write('<link href="/static/theme/common/static/css/layui-reset.css" rel="stylesheet" >');
+
+document.write('<link href="/static/theme/common/static/css/global.css" rel="stylesheet">');
+document.write('<link href="/views/themes/subVII/static/css/main.css?v='+Math.random()+'" type="text/css" rel="stylesheet" >');
+document.write('<link href="/static/plugins/loading/app.min.css?r={{.time}}" rel="stylesheet" >');
+
+
+
